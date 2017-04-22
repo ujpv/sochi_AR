931b627dbe4a4295453703ad765430709c5db1eb
Vladimir Kharitonov <ujpv@mail.ru>
2017-04-22 15:13:55 +0300

    gitignore

---
+++ src/ofApp.cpp
@@ -0,0 +1,155 @@
+#include "ofApp.h"
+#include "ofxAndroidVideoGrabber.h"
+
+//--------------------------------------------------------------
+const std::string ofApp::LOG_NAME = "Sochi_AR:";
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+    ofSetLogLevel(OF_LOG_VERBOSE);
+    ofSetOrientation(OF_ORIENTATION_90_LEFT);
+
+    ofBackground(0, 0, 0);
+
+    std::vector<ofVideoDevice> cameras = m_grabber.listDevices();
+
+    std::stringstream ss;
+    ss << LOG_NAME;
+    ss << "Found " << cameras.size() << " cameras: ";
+
+    for (ofVideoDevice &c: cameras)
+        ss << c.deviceName << "; ";
+
+    ofLog(OF_LOG_VERBOSE, ss.str());
+
+    int h = ofGetHeight();
+    int w = ofGetWidth();
+
+//    m_grabber.setPixelFormat(OF_PIXELS_RGB);
+    m_grabber.setup(h, w);
+    m_cameraImage.allocate(h, w);
+    m_grayscaleImage.allocate(h, w);
+
+    ofxAndroidVideoGrabber* androidGrabber = (ofxAndroidVideoGrabber *)m_grabber.getGrabber().get();
+    int cameraID = androidGrabber->getBackCamera();
+    if (cameraID == -1)
+        if (cameras.size())
+            cameraID = cameras.back();
+    if (cameraID == -1)
+
+
+    androidGrabber->setDeviceID(cameraID);
+    androidGrabber->setAutoFocus(true);
+    m_cameraOrientation = androidGrabber->getCameraOrientation(cameraID);
+
+    m_fern.setup("model.bmp", ofGetHeight(), ofGetWidth());
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+    m_grabber.update();
+    if (m_grabber.isFrameNew()) {
+        m_cameraImage.setFromPixels(m_grabber.getPixels());
+        m_grayscaleImage = m_cameraImage;
+        m_fern.update(m_grayscaleImage);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+//    ofPushMatrix();
+//    ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+//    ofRotate(m_cameraOrientation);
+//    ofSetRectMode(OF_RECTMODE_CENTER);
+//    m_cameraImage.draw(0, 0, m_cameraImage.getWidth(), m_cameraImage.getHeight());
+    m_fern.draw(0, 0, ofGetHeight(), ofGetWidth());
+    ofSetColor(255);
+    if (m_fern.trackedMarker.tracked)
+        m_fern.trackedMarker.draw();
+//    ofPopMatrix();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed  (int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchDown(int x, int y, int id) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchMoved(int x, int y, int id) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchUp(int x, int y, int id) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchDoubleTap(int x, int y, int id) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchCancelled(int x, int y, int id) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::swipe(ofxAndroidSwipeDir swipeDir, int id) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::pause() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::stop() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::resume() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::reloadTextures() {
+
+}
+
+//--------------------------------------------------------------
+bool ofApp::backPressed() {
+    return false;
+}
+
+//--------------------------------------------------------------
+void ofApp::okPressed() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::cancelPressed() {
+
+}
+
+void ofApp::writeLog(ofLogLevel level, const string &msg)
+{
+    ofLog(level) << LOG_NAME << msg;
+}
---
+++ src/ofApp.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxAndroid.h"
+#include "ofxFern.h"
+
+class ofApp : public ofxAndroidApp {
+
+public:
+    void setup();
+    void update();
+    void draw();
+
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void windowResized(int w, int h);
+
+    void touchDown(int x, int y, int id);
+    void touchMoved(int x, int y, int id);
+    void touchUp(int x, int y, int id);
+    void touchDoubleTap(int x, int y, int id);
+    void touchCancelled(int x, int y, int id);
+    void swipe(ofxAndroidSwipeDir swipeDir, int id);
+
+    void pause();
+    void stop();
+    void resume();
+    void reloadTextures();
+
+    bool backPressed();
+    void okPressed();
+    void cancelPressed();
+
+private:
+    void writeLog(ofLogLevel level, const std::string &msg);
+
+    ofVideoGrabber m_grabber;
+    ofxCvColorImage m_cameraImage;
+
+    ofxFern m_fern;
+
+    ofxCvGrayscaleImage m_grayscaleImage;
+
+    int m_cameraOrientation;
+
+    static const std::string LOG_NAME;
+};
